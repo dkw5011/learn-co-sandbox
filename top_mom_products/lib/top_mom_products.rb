f90e9d5d1fbbdd50dc88a77a3ee9a990deb7960f
Danielle Bergbower <dkw5011@gmail.com>
2021-04-12 03:22:08 +0000

    Automatically backed up by Learn

--- top_mom_products/lib/top_mom_products.rb
+++
@@ -1,7 +0,0 @@
-require_relative "top_mom_products/version"
-require_relative "top_mom_products/cli"
-
-module TopMomProducts
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- top_mom_products/lib/top_mom_products/version.rb
+++
@@ -1,3 +0,0 @@
-module TopMomProducts
-  VERSION = "0.1.0"
-end
